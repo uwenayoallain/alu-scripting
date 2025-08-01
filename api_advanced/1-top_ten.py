87ff2b8b491997286db844813df7a1e1f26757ab
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2025-08-01 09:18:29 +0200

    feat: add advanced api scripts

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Script to print top 10 hot posts on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
+        print("OK", end="")
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
